8d170b91d7fb5be9b8e87df128f194cb7842a354
Giorgio Andrei <cipuandrei@libero.it>
2021-07-16 15:45:18 +0200

    random-numbers-generated

---
+++ script.js
@@ -0,0 +1,16 @@
+const numbersList = [];
+console.log(numbersList);
+
+pushRandomNumbers(numbersList);
+
+function pushRandomNumbers(nlist) {//^PUSHING RANDOM NUMBERS INSIDE THE ARRAY.
+    var max = 5;
+    while (nlist.length < max) {
+        var numGenerator = Math.floor(Math.random() * 100) + 1;
+        if (!(nlist.includes(numGenerator))) {
+            nlist.push(numGenerator);
+        }
+    }
+};
+
+console.log(numbersList);
